b46fb5e88f775229c40c8ef47212f76c4a907ac0
Viktor_Viktor <life.is.life1992@gmail.com>
2015-03-30 19:59:08 +0300

    documentation added

--- cookie.js
+++ cookie.js
@@ -6,24 +6,35 @@ function writeCookie(name, value, days) {
     if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
+        /* Учебник Моррисон М.-Изучаем JavaScript(2012) предлагает устаревший метод
+         date.toGMTString(), сейчас вместо него date.toUTCString(); */
+        expires = "; expires=" + date.toUTCString();
     }
+    /*Баг в WebStorm: строка воспринимается как HTMLCollection,
+    поэтому подчеркивает. Не обращать внимания.*/
     document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function readCookie(name) {
     var searchName = name + "=";
     var cookies = document.cookie.split(";");
-    for (var i=0; i < cookies.length; i++) {
-        var c = cookies[i];
-        while (c.charAt(0) == "")
-            c = c.substring(1, c.length);
-        if (c.indexOf(searchName) == 0)
-            return c.substring(searchName.length, c.length);
+    for (var i = 0; i < cookies.length; i++) {
+        /*Добавлент .trim() так как в Chrome (Версия 41.0.2272.101 m) Windows 8
+         в переменную подтягивается куки с пробелом. */
+        var c = cookies[i].trim();
+        /* Нужно проверять длину переменной "с", иначе строка
+        while (c.charAt(0) == "") зацыкливается при отсутствии куки*/
+        if (c.length > 0) {
+            while (c.charAt(0) == "")
+                c = c.substring(1, c.length);
+            if (c.indexOf(searchName) == 0)
+                return c.substring(searchName.length, c.length);
+        }
     }
     return null;
 }
 
-function eraseCookie() {
+
+function eraseCookie(name) {
     writeCookie(name, "", -1);
 }
--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 function greetUser() {
-    var userName = readCookie("cat_username");
-    if (userName) {
-        alert("Привет," + userName + "! Я скучал по тебе");
+    if (navigator.cookieEnabled) {
+        var userName = readCookie("cat_username");
+
+        if (userName) {
+            alert("Привет," + userName + "! Я скучал по тебе");
+        }
+        else {
+            alert("Привет, я твой кот. Приятно познакомиться!");
+        }
     }
     else {
         alert("Привет, я твой кот. Приятно познакомиться!");
@@ -9,16 +15,23 @@ function greetUser() {
 }
 
 function helloText() {
-    if (userName) {
-        alert ("Ты помнишь про меня," + userName + ".Спасибо!")
+    if (navigator.cookieEnabled) {
+        var userName = readCookie("cat_username");
+        if (userName) {
+            document.getElementById("catNormal").src = "image/2.png";
+            alert ("Ты помнишь про меня," + userName + ".Спасибо!");
+        }
+        else {
+            userName = prompt("Как тебя зовут?", "Введите Ваше имя.");
+            if (userName) {
+                alert("Привет," + userName + ". Приятно познакомиться!");
+                writeCookie("cat_username", userName, 5*365);
+                document.getElementById("catNormal").src = "image/2.png";
+            }
+        }
     }
     else {
-        var userName = prompt("Как тебя зовут?", "Введите Ваше имя.");
-        if (userName) {
-            alert("Привет," + userName + ". Приятно познакомиться!");
-            writeCookie("cat_username", userName, 5*365);
-            document.getElementById("catNormal").src = "image/2.png";
-        }
+        alert("Извините ваш браузер не поддерживает cookie");
     }
 }
 
@@ -42,5 +55,7 @@ setTimeout(askWhereAreYou, 8*1000);
 var pageRefresh = setInterval(reloadPage, 5*1000);
 
 function resizeCat() {
+    /* Для корректной работы нужно обязательно добавить единицы измерения ("px"),
+    в книге Моррисон М.-Изучаем JavaScript(2012) это не указано */
     document.getElementById("catNormal").style.height = document.body.clientHeight * 0.6 + "px";
 }
